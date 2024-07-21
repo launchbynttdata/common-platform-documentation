d4e5cde5277bc3343da124d230cb2d614ec109f4
aaron-christian-nttd <aaron.christian@nttdata.com>
2024-07-21 17:32:39 +0000

    more info added in guides

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Launch DevSecOps `common-platform-documentation`
 
+This repository is a knowledge base repository containing information related to, and associated with, the `Launch` platform.
+
 The `Launch` platform is a DevSecOps platform that integrates security in your development and operations process. It streamlines the creation and management of Infrastructure as Code (IaC) using Terraform and Terragrunt, and automates deployments through secure efficient pipelines. The `Launch` platform integrates with your SCM of choice utilizing webhooks and allows you to utilize the platform on a variety of cloud providers. The `launch-cli` tool enables local repository creation and lifecycle management of your service.
 
 ## Supported platforms
@@ -21,16 +23,19 @@ The following platforms and environments are currently supported by the `Launch`
 
 This documentation is filled with guides and information related to developing with the `Launch` platform. 
 
-### Quick Start Guides:
-- [Setting up local environment - Mac]()
+### Environment Guides
+- Setting up local environment: [Mac](#), [Windows](#)
 - [Setting up Visual Studio Code](./platform/development-environments/local/vscode/README.md)
 - [Setting up Visual Studio Code dev containers](./platform/development-environments/local/vscode/dev-containers/README.md)
+
+### Quick Start Guides:
 - [Deploy a full stack Java application on AWS ECS](./platform/cli/usage-guides/new-service/java/aws/README.md)
 
-
-### Guides and Tutorials
+### Usage-guides and Tutorials
 - [Using the `launch-cli`](./platform/cli/usage-guides/README.md)
-- [Setting up local development environments]()
 
 
 ## License
+Unless explicitly stated otherwise, the content of the "common-platform-documentation" repository is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (CC BY-NC-ND 4.0).
+
+[Read Full License](./LICENSE)
--- platform/development-environments/local/vscode/dev-containers/files/dev.postCreate.sh
+++ platform/development-environments/local/vscode/dev-containers/files/dev.postCreate.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-echo 'export GITHUB_TOKEN="<PAT_HERE>"' >> ~/.bashrc
+local work_dir="/workspaces/workplace"
+local git_token="<PAT_HERE>"
+local sso_aws_url=""
+local sso_aws_account_id=""
+local sso aws_region="us-east-1"
+local aws_root_account_id=""
+local aws_sandbox_account_id=""
+local aws_region="us-east-2"
+
+# Set your Git token
+echo 'export GITHUB_TOKEN="'${git_token}'"' >> ~/.bashrc
 
 # Local user scripts to added to PATH for execution
-echo 'export PATH="/workspaces/workplace/.localscripts:${PATH}"' >> ~/.bashrc
+echo 'export PATH="'${work_dir}'/.localscripts:${PATH}"' >> ~/.bashrc
 
 # Install repo
 mkdir -p ~/.bin
@@ -14,14 +24,15 @@ chmod a+rx ~/.bin/repo
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
 
-# install Launch CLI as a dev dependency
+# Install Launch CLI as a dev dependency
 python -m pip install launch-cli
+
 ## Uncomment the following lines if wish to have a local 
 ## dev source as the install folder
-# cd /workspaces/workplace/launch-cli
+# cd ${work_dir}/launch-cli
 # python -m pip install -e '.[dev]'
 # python3.12 -m pip install boto3
-# cd /workspaces/workplace/
+# cd ${work_dir}
 
 # Set up netrc
 echo "Setting ~/.netrc variables"
@@ -40,40 +51,40 @@ git config --global push.autoSetupRemote true
 echo 'alias git_sync="git pull origin main"' >> ~/.bashrc
 echo 'alias git_boop="git reset --soft HEAD~1"' >> ~/.bashrc
 
-# for dir in /workspaces/workplace/*; do (cd "$dir" && git_sync && make configure); done
+# Set up AWS Config
 mkdir -p ~/.aws
 echo "
 [default]
-region = us-east-2
+region = ${aws_region}
 output = json
 
 [profile launch]
-sso_start_url = <REPLACE_WITH_YOUR_URL>
-sso_region = us-east-1
+sso_start_url = ${sso_aws_url}
+sso_region = ${aws_region}
 sso_account_name = Launch DSO
-sso_account_id = <REPLACE_WITH_YOUR_ACCOUNT_ID>
+sso_account_id = ${sso_aws_account_id}
 sso_role_name = AdministratorAccess
-region = us-east-2
+region = ${aws_region}
 credential_process = aws-sso-util credential-process --profile launch
 sso_auto_populated = true
 
 [profile launch-root-admin]
-sso_start_url = <REPLACE_WITH_YOUR_URL>
-sso_region = us-east-1
-sso_account_name = Launch DSO
-sso_account_id = <REPLACE_WITH_YOUR_SANDBOX_ACCOUNT_ID>
+sso_start_url = ${sso_aws_url}
+sso_region = ${aws_region}
+sso_account_name = Launch Root Account
+sso_account_id = ${aws_root_account_id}
 sso_role_name = AdministratorAccess
-region = us-east-2
+region = ${aws_region}
 credential_process = aws-sso-util credential-process --profile launch
 sso_auto_populated = true
 
 [profile launch-sandbox-admin]
-sso_start_url = <REPLACE_WITH_YOUR_URL>
-sso_region = us-east-1
-sso_account_name = Launch DSO
-sso_account_id = <REPLACE_WITH_YOUR_ROOT_ACCOUNT_ID>
+sso_start_url = ${sso_aws_url}
+sso_region = ${aws_region}
+sso_account_name = Launch Sandbox Account
+sso_account_id = ${aws_sandbox_account_id}
 sso_role_name = AdministratorAccess
-region = us-east-2
+region = ${aws_region}
 credential_process = aws-sso-util credential-process --profile launch
 sso_auto_populated = true
 " >> ~/.aws/config
